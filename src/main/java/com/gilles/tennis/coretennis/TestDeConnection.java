8f0bbe4379a6d81db5e91157e479694c8dee8923
gillios <ngondogillespatrick@gmail.com>
2021-03-26 05:03:13 -0400

    First Conection JDBC

--- src/main/java/com/gilles/tennis/coretennis/TestDeConnection.java
+++ src/main/java/com/gilles/tennis/coretennis/TestDeConnection.java
@@ -5,14 +5,38 @@
  */
 package com.gilles.tennis.coretennis;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestDeConnection {
 
-    
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        try {
+
+
+            // MySQL driver MySQL Connector
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/TENNIS", "gilles", "gillespatr9ck");
+
+
+            System.out.println("success");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
 }
