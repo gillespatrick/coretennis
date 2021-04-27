53560a181e3681a83a6b325b183ccd96b6965ff9
gillios <ngondogillespatrick@gmail.com>
2021-04-27 05:47:11 -0400

    3- Rapid entity mapping

--- src/main/java/com/gilles/tennis/coretennis/entity/Joueur.java
+++ src/main/java/com/gilles/tennis/coretennis/entity/Joueur.java
@@ -1,8 +1,17 @@
 package com.gilles.tennis.coretennis.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Joueur {
 
     // Attibuts
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private String prenom;
